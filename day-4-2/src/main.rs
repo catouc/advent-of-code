ccd4769b4c73ed790a6a957b39c1c978431f6dfa
Philipp Böschen <catouc@philipp.boeschen.me>
2022-12-04 11:12:21 +0100

    Day 4 - this was fun

---
+++ day-4-2/src/main.rs
@@ -0,0 +1,44 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let lines = match read_lines("./input") {
+        Ok(lines) => lines,
+        Err(err) => panic!("Could not read lines: {}", err),
+    };
+
+    let mut contained_count = 0;
+    for line in lines {
+        if let Ok(sections) = line {
+            let split: Vec<&str> = sections.split(",").collect();
+            let contained = is_contained(split[0], split[1]);
+            if contained {
+                contained_count += 1;
+            }
+        }
+    }
+
+    println!("{}", contained_count);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+// section 1 lower x, upper y
+// section 2 lower a, upper b
+// section 1 is fully in section 2 if x >= a && y <= b
+// section 1 is overlapping with section 2 if x >= a && x <= b || y <= b && y >= a
+fn is_contained(a: &str, b: &str) -> bool {
+    let (a_lower, a_upper) = split_section(a);
+    let (b_lower, b_upper) = split_section(b);
+    ((a_lower >= b_lower && a_lower <= b_upper) || (a_upper <= b_upper && a_upper >= b_lower)) || ((b_lower >= a_lower && b_lower <= a_upper) || (b_upper <= a_upper && b_upper >= a_lower))
+}
+
+fn split_section(section: &str) -> (i32, i32) {
+    let split: Vec<i32> = section.split("-").map(|x|x.parse().unwrap()).collect();
+    (split[0], split[1])
+}
